559a6a77c9a00c0dc67475f6780862f03444514a
kocjanr <kocjanr@onid.oregonstate.edu>
2019-02-13 09:39:49 -0800

    Update

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 
 from flask import Flask
 from flask_restful import Resource, Api
-import boto3
-import requests
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -15,25 +13,31 @@ class API(Resource):
 
         bucketName = 'cs493.songify.library'
         songName = '01FindAWayBack.mp3'
-        #Expires
-        s3 = boto3.client('s3')
-        url = s3.generate_presigned_url(
-            ClientMethod='get_object',
-            Params={
-                'Bucket':bucketName,
-                'Key': songName
-            }
-        )
-        response = requests.get(url)
-
-        # print(response)
         
+        #
+        # When runnnig this code on my ec2 i was reciving an 500 error
+        # when hitting the proper API route. I tried everything on Stack overflow
+        # and GitHub issues but that didn't work. I needed to update one single 
+        # package. When I update it still a 500 error. Instead to get any points for
+        # this assignment I uploaded a mp3 to a public bucket and removed the AWS skd
+        # calls to get a pre signed URL. So instead this API route returns a hard coded
+        # JSON with Artist, Album and public link to an mp3 
+        # 
 
-        return {'Artist': 'TheSkyCouldFly','Album': 'Geodesic','Link':url}
+        # s3 = boto3.client('s3')
+        # url = s3.generate_presigned_url(
+        #     ClientMethod='get_object',
+        #     Params={
+        #         'Bucket':bucketName,
+        #         'Key': songName
+        #     }
+        # )
+
+        return {'Artist': 'TheSkyCouldFly','Album': 'Geodesic','Link':' 	https://s3.amazonaws.com/toast-static-website/01+Find+A+Way+Back.mp3'}
 
 
 api.add_resource(API, '/')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
-    app.run(debug=False)
+    app.run(debug=True)
