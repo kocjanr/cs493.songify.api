5e6126bede6436b41a93404e56d3748f10b8496c
kocjanr <kocjanr@onid.oregonstate.edu>
2019-02-13 06:56:13 -0800

    un commented aws skd calls

--- app.py
+++ app.py
@@ -13,27 +13,27 @@ CORS(app)
 class API(Resource):
     def get(self):
 
-        # bucketName = 'cs493.songify.library'
-        # songName = '01FindAWayBack.mp3'
-        # #Expires
-        # s3 = boto3.client('s3')
-        # url = s3.generate_presigned_url(
-        #     ClientMethod='get_object',
-        #     Params={
-        #         'Bucket':bucketName,
-        #         'Key': songName
-        #     }
-        # )
-        # response = requests.get(url)
+        bucketName = 'cs493.songify.library'
+        songName = '01FindAWayBack.mp3'
+        #Expires
+        s3 = boto3.client('s3')
+        url = s3.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={
+                'Bucket':bucketName,
+                'Key': songName
+            }
+        )
+        response = requests.get(url)
 
         # print(response)
         
 
-        return {'Artist': 'TheSkyCouldFly','Album': 'Geodesic','Link':'https://s3.amazonaws.com/toast-static-website/01+Find+A+Way+Back.mp3'}
+        return {'Artist': 'TheSkyCouldFly','Album': 'Geodesic','Link':url}
 
 
 api.add_resource(API, '/')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
-    app.run(debug=True)
+    app.run(debug=False)
